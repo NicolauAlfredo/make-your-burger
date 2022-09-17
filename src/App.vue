a029db02d221d3a334a12cf5132942eb8e78e1be
Nicolau Alfredo <nicolaualfredoalfredo@gmail.com>
2022-09-17 23:02:29 +0100

    Estilizando as views

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <Navbar />
+  <Navbar :logo='logo_src' :alt='app_name'/>
   <router-view/>
 
-  <Footer />
+  <Footer/>
 </template>
 
 <script>
@@ -13,10 +13,37 @@ export default {
     components: {
         Navbar,
         Footer
+    },
+    data() {
+      return {
+        logo_src: '/img/logo.png',
+        app_name: 'Make Your Burger'
+      }
     }
 }
 </script>
 
-<style>
-     
+<style >
+  /* Configurações gerais */
+     * {
+      font-family: Helvetica;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+     }
+
+
+  /* Configurações das views */
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+    <div id="footer">
+        <p>Make Your Burger &copy; 2022 by <a href="#">Nicolau Alfredo</a></p>
+    </div>
 </template>
 
 <script>
@@ -7,3 +9,24 @@
         name: 'Footer'
     }
 </script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center; 
+        width: 100%;
+    }
+
+    a { 
+        text-decoration: none;
+        background: #FCBA03;
+        padding: 10px;
+        color: #000;
+        border-radius: 5px;
+    }
+</style>
